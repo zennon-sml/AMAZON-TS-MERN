cffdacd426ae03b803616c02a7fef90edb39ad52
zennon-sml <zennonsampaioml@gmail.com>
2024-01-19 11:44:53 -0300

    instaled react query tanstack

---
+++ frontend/src/hooks/productHooks.ts
@@ -0,0 +1,8 @@
+import { useQuery } from "@tanstack/react-query";
+import apiClient from "../apiClient";
+import { Product } from "../types/Product";
+
+export const useGetProductsQuery = () => useQuery({
+    queryKey: ['products'],
+    queryFn: async () => (await apiClient.get<Product[]>(`api/products`)).data
+})
